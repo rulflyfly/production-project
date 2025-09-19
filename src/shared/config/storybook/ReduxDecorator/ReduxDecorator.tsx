95c1f2bbe325a96541e414073a4bd2559d044225
rulflyfly <rulflyfly@gmail.com>
2025-09-19 18:02:22 +0300

    chore: add pre commit

--- src/shared/config/storybook/ReduxDecorator/ReduxDecorator.tsx
+++ src/shared/config/storybook/ReduxDecorator/ReduxDecorator.tsx
@@ -3,7 +3,7 @@ import { StoreProvider } from 'app/providers/StoreProvider';
 import 'app/styles/index.scss';
 
 export const ReduxDecorator = (story: () => Story) => (
-    <StoreProvider initialState={{ counter: { value: 10 } }}>
+    <StoreProvider initialState={{ counter: { value: 10 }, user: {} }}>
         {story()}
     </StoreProvider>
 );
