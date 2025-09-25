1002c8935777c943af07339630f643990ffe36f1
rulflyfly <rulflyfly@gmail.com>
2025-09-25 22:58:22 +0300

    fix: unit test

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -20,26 +20,24 @@ describe('loginByUsername.test', () => {
 
     test('login success', async () => {
         const userValue = { username: '123', id: '1' };
-        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
 
         const thunk = new TestAsyncThunk(loginByUserName);
+        thunk.api.post.mockResolvedValue({ data: userValue });
         const result = await thunk.callThunk({ username: '123', password: '123' });
 
         expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
         expect(thunk.dispatch).toHaveBeenCalledTimes(3);
-        expect(mockedAxios.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('fulfilled');
         expect(result.payload).toEqual(userValue);
     });
 
     test('login error', async () => {
-        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const thunk = new TestAsyncThunk(loginByUserName);
+        thunk.api.post.mockRejectedValue({ response: { status: 403 } });
         const result = await thunk.callThunk({ username: '123', password: '123' });
 
         expect(thunk.dispatch).toHaveBeenCalledTimes(2);
 
-        expect(mockedAxios.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('rejected');
         expect(result.payload).toBe('You entered wrong username or password');
     });
--- src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
+++ src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
@@ -9,17 +9,23 @@ export class TestAsyncThunk<Return, Arg, RegectValue> {
 
     getState: () => StateSchema;
 
+    api: { post: jest.Mock };
+
+    navigate: jest.Mock;
+
     actionCreator: ActionCreatorType<Return, Arg, RegectValue>;
 
     constructor(actionCreator: ActionCreatorType<Return, Arg, RegectValue>) {
         this.actionCreator = actionCreator;
         this.dispatch = jest.fn();
         this.getState = jest.fn();
+        this.api = { post: jest.fn() };
+        this.navigate = jest.fn();
     }
 
     async callThunk(arg: Arg) {
         const action = this.actionCreator(arg);
-        const result = await action(this.dispatch, this.getState, undefined);
+        const result = await action(this.dispatch, this.getState, { api: this.api, navigate: this.navigate });
 
         return result;
     }
